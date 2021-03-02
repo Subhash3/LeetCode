0005ebefc19a0916bd4df0e0702fee170d795bfc
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-03-02 15:25:34 +0530

    Mar-2021, Week-1, Day-2; Set mismatch added another solution

--- Mar-2021/Week-1/set-mismatch/Solution.cpp
+++ Mar-2021/Week-1/set-mismatch/Solution.cpp
@@ -8,9 +8,9 @@ class Solution
 public:
     vector<int> findErrorNums(vector<int> &nums)
     {
-        int i, n;
+        int i, n, allXor = 0, numsXor = 0;
+        int duplicate = -1, missing = -1;
         vector<int> freq;
-        vector<int> result = {-1, -1}; // {duplicate, missing}
 
         n = nums.size();
         freq = vector<int>(n);
@@ -18,26 +18,47 @@ public:
         for (i = 0; i < n; i++)
         {
             freq[nums[i] - 1] += 1;
-        }
-        for (i = 0; i < n; i++)
-        {
-            printf("%d : %d\n", i + 1, freq[i]);
-        }
-        printf("\n");
-
-        for (i = 0; i < n; i++)
-        {
-            if (freq[i] > 1)
+            if (freq[nums[i] - 1] > 1)
             {
-                result[0] = i + 1;
+                duplicate = nums[i];
             }
-
-            else if (freq[i] == 0)
-            {
-                result[1] = i + 1;
-            }
+            numsXor ^= (nums[i]);
+            allXor ^= (i + 1);
         }
+        // Eliminate duplicate from numsXor
+        numsXor ^= duplicate;
+        missing = numsXor ^ allXor;
 
-        return result;
+        return {duplicate, missing};
     }
 };
+
+/*
+    Don't worry about the duplicate for now.
+    let n = 4 => arr = [1, 2, 3, 4]
+    Let 3 be the missing number
+    nums = [1, 2, 4]
+
+    missing can be calculated as follows
+    (Xor of original arr) XOR (Xor of nums arr)
+
+    a = XOR of original array = 1 ^ 2 ^ 3 ^ 4 ---(1)
+    b = XOR of given array    = 1 ^ 2 ^ 4     ---(2)
+
+    a XOR b = (1 ^ 2 ^ 3 ^ 4) ^ (1 ^ 2 ^ 4)   ---(3)
+
+    This eliminates duplicates and leaves us with the missing number which is 3.
+
+    But inorder to find the missing, we need to find the duplicate.
+    This can be done in O(N) complexity with a hashmap.
+    For the given example, duplicate = 2
+
+    This time Xor of given array contains the duplicate.
+    => b = XOR of given array    = 1 ^ 2 ^ 2 ^ 4.
+    But we want it without duplicate. Hence, just XOR it with the duplicate since it eliminates one instance of that duplicate.
+    => b = b XOR duplicate
+    => b = (1 ^ 2 ^ 2 ^ 4) XOR (2)
+    => b = (1 ^ 2 ^ 4)
+
+    Now we just have to do (3) to get the missing element.
+*/
